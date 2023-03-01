363eb899928daf1c86ee5699dae164cc3bec3415
HoaiPhuoGn <92800582+HoaiPhuoGn@users.noreply.github.com>
2023-03-01 14:14:42 +0700

    first commit

---
+++ Course_Online_Backend/src/main/java/fa/training/backend/CourseOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package fa.training.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseOnlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseOnlineBackendApplication.class, args);
+	}
+
+}
